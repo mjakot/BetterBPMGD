f8db6cd14fe7e452174f0c4786785b2e8b74f8af
Mjakot <hlebletjaslomaju@gmail.com>
2023-11-28 23:00:34 +0200

    Did something

--- BetterBPMGD/Commands/CloseViewCommand.cs
+++ BetterBPMGD/Commands/CloseViewCommand.cs
@@ -1,12 +1,20 @@
 ﻿using BetterBPMGD.ViewModels;
+using System;
 
 namespace BetterBPMGD.Commands
 {
     public class CloseViewCommand : CommandBase
     {
+        private readonly Action closuPopup;
+
+        public CloseViewCommand(Action closuPopup)
+        {
+            this.closuPopup = closuPopup;
+        }
+
         public override void Execute(object? parameter)
         {
-            throw new System.NotImplementedException();
+            closuPopup();
         }
     }
 }
--- BetterBPMGD/ViewModels/HelpViewModel.cs
+++ BetterBPMGD/ViewModels/HelpViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace BetterBPMGD.ViewModels
+﻿using BetterBPMGD.Commands;
+using System.Windows.Input;
+
+namespace BetterBPMGD.ViewModels
 {
     public class HelpViewModel : ViewModelBase
     {
@@ -6,9 +9,17 @@
 
         public string HelpText => helpText;
 
+        public ICommand CloseHelpPopupCommand { get; }
+
         public HelpViewModel(string helpText)
         {
             this.helpText = helpText;
+            CloseHelpPopupCommand = new CloseViewCommand(ClosePopup);
+        }
+
+        private void ClosePopup()
+        {
+
         }
     }
 }
