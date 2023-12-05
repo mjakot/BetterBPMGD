5fe56dbe96df9a312972775e320687c9a8c449ae
Mjakot <hlebletjaslomaju@gmail.com>
2023-12-05 22:22:00 +0200

    Added LevelsSaveCipher.cs

---
+++ BetterBPMGDCLI/Models/LevelsSaveCipher.cs
@@ -0,0 +1,17 @@
+﻿namespace BetterBPMGDCLI.Models
+{
+    public class LevelsSaveCipher
+    {
+        private byte[] levelsSaveData;
+
+        public byte[] LevelsSaveData => levelsSaveData;
+
+        public LevelsSaveCipher(byte[] levelsSaveData) => this.levelsSaveData = levelsSaveData;
+
+        public LevelsSaveCipher XOR(int key) => throw new NotImplementedException();
+
+        public LevelsSaveCipher Base64(bool decode) => throw new NotImplementedException();
+
+        public LevelsSaveCipher GZIP(bool fold) => throw new NotImplementedException();
+    }
+}
