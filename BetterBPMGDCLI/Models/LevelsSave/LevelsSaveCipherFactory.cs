c8bce523d52855a81ecb06da917c5221b974f98a
Mjakot <hlebletjaslomaju@gmail.com>
2023-12-06 21:31:29 +0200

    Added LevelsSaveCipherFactory

---
+++ BetterBPMGDCLI/Models/LevelsSave/LevelsSaveCipherFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace BetterBPMGDCLI.Models.LevelsSave
+{
+    public class LevelsSaveCipherFactory
+    {
+        public LevelsSaveCipher Decode(LevelsSaveCipher data) => data.XOR(11).Base64(true).GZIP(false);
+
+        public LevelsSaveCipher Encode(LevelsSaveCipher data) => throw new NotImplementedException();
+    }
+}
