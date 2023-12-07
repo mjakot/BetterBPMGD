16737a8572dcbed52789b75a1573a5e06eff98e7
Mjakot <hlebletjaslomaju@gmail.com>
2023-12-07 20:07:42 +0200

    Implemented XOR in LevelsSaveCipher

--- BetterBPMGDCLI/Models/LevelsSave/LevelsSaveCipher.cs
+++ BetterBPMGDCLI/Models/LevelsSave/LevelsSaveCipher.cs
@@ -8,7 +8,12 @@
 
         public LevelsSaveCipher(byte[] levelsSaveData) => this.levelsSaveData = levelsSaveData;
 
-        public LevelsSaveCipher XOR(int key) => throw new NotImplementedException();
+        public LevelsSaveCipher XOR(int key)
+        {
+            for (int i = 0; i < levelsSaveData.Length; i++) levelsSaveData[i] = (byte)(levelsSaveData[i] ^ key);
+
+            return new(levelsSaveData);
+        }
 
         public LevelsSaveCipher Base64(bool decode) => throw new NotImplementedException();
 
