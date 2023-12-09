06de6c95fb37b9aea3f0d2525a9a4ccf4213922a
Mjakot <hlebletjaslomaju@gmail.com>
2023-12-09 19:50:53 +0200

    Implemented Cache

--- BetterBPMGDCLI/Utils/Cache.cs
+++ BetterBPMGDCLI/Utils/Cache.cs
@@ -4,19 +4,58 @@ namespace BetterBPMGDCLI.Utils
 {
     public class Cache
     {
+        //TODO: get path from settings
+        private const string CachePath = "levels.xml";
+
         public static SaveCacheResult SaveCache(XmlDocument xmlCache)
         {
-            Save();
+            string stringCahche = string.Empty;
+
+            using (var stringWriter = new StringWriter())
+            using (var xmlTextWriter = XmlWriter.Create(stringWriter))
+            {
+                xmlCache.WriteTo(xmlTextWriter);
+                xmlTextWriter.Flush();
+                stringCahche = stringWriter.GetStringBuilder().ToString();
+            }
+
+            return SaveCache(stringCahche);
         }
 
-        public static SaveCacheResult SaveCache(string stringCache)
+        public static SaveCacheResult SaveCache(string stringCache) => Save(stringCache);
+
+        public static XmlDocument GetCacheXmlDocument()
         {
-            Save();
+            XmlDocument xmlCache = new XmlDocument();
+
+            string stringCache = GetCacheString() ?? string.Empty;
+
+            xmlCache.LoadXml(stringCache);
+
+            return xmlCache;
         }
 
-        private static bool Save()
+        public static string? GetCacheString() => Get();
+
+        private static SaveCacheResult Save(string cache)
         {
-            throw new NotImplementedException();
+            if (cache == File.ReadAllText(CachePath)) return SaveCacheResult.AlreadySaved;
+
+            try
+            {
+                File.WriteAllText(CachePath, cache);
+
+                return SaveCacheResult.Success;
+            }
+            catch (Exception)
+            {
+                return SaveCacheResult.Fail;
+            }
+        }
+
+        private static string? Get()
+        {
+            return string.Empty;
         }
     }
 }
--- BetterBPMGDCLI/Utils/SaveCacheResult.cs
+++ BetterBPMGDCLI/Utils/SaveCacheResult.cs
@@ -4,7 +4,6 @@
     {
         Success,
         Fail,
-        Updated,
         AlreadySaved
     }
 }
