af04a7f4ba5fce029bbdfa7f420766b578001e31
Mjakot <hlebletjaslomaju@gmail.com>
2023-12-09 19:53:44 +0200

    LocalLevelsCipherFactory is now saving cache

--- BetterBPMGDCLI/Models/LevelsSave/Factories/LocalLevelsCipherFactory.cs
+++ BetterBPMGDCLI/Models/LevelsSave/Factories/LocalLevelsCipherFactory.cs
@@ -1,8 +1,21 @@
-﻿namespace BetterBPMGDCLI.Models.LevelsSave.Factories
+﻿using BetterBPMGDCLI.Utils;
+
+namespace BetterBPMGDCLI.Models.LevelsSave.Factories
 {
     public class LocalLevelsCipherFactory
     {
-        public static LocalLevelsCipher Decode(LocalLevelsCipher data) => data.XOR(11).FromBase64UrlToBase64().FromBase64ToByteArray().GZIPDecompress();
+        //TODO: move cache saving in different place
+        public static LocalLevelsCipher Decode(LocalLevelsCipher data)
+        {
+            LocalLevelsCipher result = data.XOR(11)
+                .FromBase64UrlToBase64()
+                .FromBase64ToByteArray()
+                .GZIPDecompress();
+
+            Cache.SaveCache(result.LocalLevelsString);
+
+            return result;
+        }
 
         //Geometry dash actually encrypts xml files by itself so there is no need for Encrypt()
     }
