17372af33f57058c2188d4b15ed39936da970999
Mjakot <hlebletjaslomaju@gmail.com>
2023-12-09 18:28:43 +0200

    Changed comment

--- BetterBPMGDCLI/Models/LevelsSave/LocalLevelsCipherFactory.cs
+++ BetterBPMGDCLI/Models/LevelsSave/LocalLevelsCipherFactory.cs
@@ -4,6 +4,6 @@
     {
         public static LocalLevelsCipher Decode(LocalLevelsCipher data) => data.XOR(11).FromBase64UrlToBase64().FromBase64ToByteArray().GZIPDecompress();
 
-        //Geometry dash actually encrypts xml files by itself, so this is not really useful
+        //Geometry dash actually encrypts xml files by itself so Encrypt() method is not really useful
     }
 }
