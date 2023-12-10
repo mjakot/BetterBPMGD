e8ccd2c74d04d1f3320daac5d5c8186f5774fba9
Mjakot <hlebletjaslomaju@gmail.com>
2023-12-10 17:25:31 +0200

    Added GuidelineColors

---
+++ BetterBPMGDCLI/Models/LevelsSave/Level/GuidelineColors.cs
@@ -0,0 +1,10 @@
+﻿namespace BetterBPMGDCLI.Models.LevelsSave.Level
+{
+    public struct GuidelineColors
+    {
+        public const double None = 0.0;
+        public const double Orange = 0.8;
+        public const double Yellow = 0.9;
+        public const double Green = 1.0;
+    }
+}
