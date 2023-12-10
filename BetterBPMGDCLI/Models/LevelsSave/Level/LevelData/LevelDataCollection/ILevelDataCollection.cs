f2e16b0380f6c00cf939037959a779df1001231f
Mjakot <hlebletjaslomaju@gmail.com>
2023-12-10 17:41:51 +0200

    Added ILevelDataCollection

---
+++ BetterBPMGDCLI/Models/LevelsSave/Level/LevelData/LevelDataCollection/ILevelDataCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace BetterBPMGDCLI.Models.LevelsSave.Level.LevelData.LevelDataCollection
+{
+    public interface ILevelDataCollection : ICollection
+    {
+        public List<ILevelData> List { get; }
+
+        public string Encode();
+    }
+}
--- BetterBPMGDCLI/Models/LevelsSave/Level/LocalLevelData.cs
+++ BetterBPMGDCLI/Models/LevelsSave/Level/LocalLevelData.cs
@@ -1,7 +1,9 @@
-﻿namespace BetterBPMGDCLI.Models.LevelsSave.Level
+﻿using BetterBPMGDCLI.Models.LevelsSave.Level.LevelData;
+
+namespace BetterBPMGDCLI.Models.LevelsSave.Level
 {
     public class LocalLevelData : Common.Level
     {
-
+        public ILevelData 
     }
 }
