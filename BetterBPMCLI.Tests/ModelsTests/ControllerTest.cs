3426564c0bdd3806d39cd83526efcc1420544ebd
Mjakot <hlebletjaslomaju@gmail.com>
2023-12-15 15:38:19 +0200

    Cache now has async methods

--- BetterBPMCLI.Tests/ModelsTests/ControllerTest.cs
+++ BetterBPMCLI.Tests/ModelsTests/ControllerTest.cs
@@ -1,8 +1,43 @@
-﻿namespace BetterBPMCLI.Tests.ModelsTests
+﻿using BetterBPMGDCLI.Models;
+using BetterBPMGDCLI.Models.LevelsSave.Level;
+using BetterBPMGDCLI.Models.Settings;
+
+namespace BetterBPMCLI.Tests.ModelsTests
 {
     public class ControllerTest
     {
         [Fact]
-        public void LoadLocalLevel_
+        public async Task LoadLocalLevel_ValidXML_ReturnsValidLocalLevel()
+        {
+            Controller controller = new(new TestSettings());
+
+
+
+            await controller.LoadLocalLevelAsync("k_0");
+
+            LocalLevel? localLevel = controller.Level;
+
+            string? actual = localLevel?.LevelData.LevelData;
+
+
+
+            Assert.Equal("", actual);
+        }
+
+        private class TestSettings : ISettings
+        {
+            public string DefaultLevelsSavePath => @"path/to/levels/save";
+
+            public string LevelsSavePath { get => DefaultLevelsSavePath; set => throw new NotImplementedException(); }
+
+            public string DefaultCachePath => @"path/to/cache";
+
+            public string CachePath { get => DefaultCachePath; set => throw new NotImplementedException(); }
+
+            public void ResetAllSettings()
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
