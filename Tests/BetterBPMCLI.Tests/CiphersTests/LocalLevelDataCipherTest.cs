079f408ae8bd84a467a0860e19d57b7d32831192
Mjakot <hlebletjaslomaju@gmail.com>
2024-01-17 15:54:11 +0200

    Tested LocalLevelDataCipher

---
+++ Tests/BetterBPMCLI.Tests/CiphersTests/LocalLevelDataCipherTest.cs
@@ -0,0 +1,24 @@
+﻿using BetterBPMGDCLI.Models.Ciphers;
+
+namespace BetterBPMCLI.Tests.CiphersTests
+{
+    public class LocalLevelDataCipherTest
+    {
+        const string MINIMALLOCALLEVELDATA = """H4sIAAAAAAAAC6WTwQ3DMAwDF3IBUZLTBH1lhg7AAbJCh29k5pmgLfohYVM-Swa8PWNuYBqd8M6g905A5jJtJm_gRJgZ7wTRS2YaZ-IFDoT5dwj8j1hOEVWjA19BnHX-DPTTk-zr_xl5ykDUQKUx9MNA_XKgH19mugC1bUU0K-uySZZt16lh1_tQP3KtMNfqGcvY9KHijGDNoUphKlKVq8xV4cKlySBz3SiUixLKQlmozRQsBYtQljI1HLqhPkbZovaPvkXxo0Go3d0eby5QMLtKAwAA""";
+        const string MINIMALLOCALLEVELDATADECRYPTED = """kS38,1_40_2_125_3_255_11_255_12_255_13_255_4_-1_6_1000_7_1_15_1_18_0_8_1|1_0_2_102_3_255_11_255_12_255_13_255_4_-1_6_1001_7_1_15_1_18_0_8_1|1_0_2_102_3_255_11_255_12_255_13_255_4_-1_6_1009_7_1_15_1_18_0_8_1|1_255_2_255_3_255_11_255_12_255_13_255_4_-1_6_1002_5_1_7_1_15_1_18_0_8_1|1_40_2_125_3_255_11_255_12_255_13_255_4_-1_6_1013_7_1_15_1_18_0_8_1|1_40_2_125_3_255_11_255_12_255_13_255_4_-1_6_1014_7_1_15_1_18_0_8_1|1_135_2_135_3_135_11_255_12_255_13_255_4_-1_6_1005_5_1_7_1_15_1_18_0_8_1|1_255_2_255_3_255_11_255_12_255_13_255_4_-1_6_1006_5_1_7_1_15_1_18_0_8_1|,kA13,0,kA15,0,kA16,0,kA14,,kA6,1,kA7,1,kA25,0,kA17,1,kA18,0,kS39,0,kA2,0,kA3,0,kA8,0,kA4,0,kA9,0,kA10,0,kA22,0,kA23,0,kA24,0,kA27,1,kA40,1,kA41,1,kA42,1,kA28,0,kA29,0,kA31,1,kA32,1,kA36,0,kA43,0,kA44,0,kA33,1,kA34,1,kA35,0,kA37,1,kA38,1,kA39,1,kA19,0,kA26,0,kA20,0,kA21,0,kA11,0;""";
+
+        [Fact]
+        public void Decode_MinimalLocalLevelData_DecodedMinimalLocalLevelData()
+        {
+            LocalLevelDataCipher cipher = new(MINIMALLOCALLEVELDATA);
+
+
+
+            string actual = cipher.Decode();
+
+
+
+            Assert.Equal(MINIMALLOCALLEVELDATADECRYPTED, actual);
+        }
+    }
+}
