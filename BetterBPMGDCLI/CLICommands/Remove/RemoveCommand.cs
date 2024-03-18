5bba675b12c023d6943d6ca9a10ea91944af7e08
Mjakot <hlebletjaslomaju@gmail.com>
2024-03-18 21:16:11 +0200

    .

---
+++ BetterBPMGDCLI/CLICommands/Remove/RemoveCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.CommandLine;
+
+namespace BetterBPMGDCLI.CLICommands
+{
+    public class RemoveCommand(ICommand[] removeCommands) : HostCommandBase, ICommand
+    {
+        private readonly ICommand[] removeCommands = removeCommands;
+
+        public Command BuildCommand() => BuildCommand<RemoveCommand>(removeCommands);
+    }
+}
